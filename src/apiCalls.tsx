88076cd6a6c2a87c59a8124364504c9014682621
Yiran Li <yiran.li@hotmail.com>
2020-12-28 15:30:22 -0700

    Add removeTodoList function

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -12,4 +12,40 @@ export const apiCalls = {
       }
     );
   },
+
+  addTodoList(data: { id: string; text: string; complete: boolean }) {
+    return fetch('https://todo-list-yl.herokuapp.com/todo-list', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    }).then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(
+          'Sorry we are having difficulty processing this request, please try again later!'
+        );
+      }
+    });
+  },
+
+  removeTodoList(id: string) {
+    return fetch('https://todo-list-yl.herokuapp.com/todo-list', {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id: id }),
+    }).then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(
+          'Sorry we are having difficulty processing this request, please try again later!'
+        );
+      }
+    });
+  },
 };
